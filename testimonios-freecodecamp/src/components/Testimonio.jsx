fd372f7ed24d4abef4c2d0236393eccf73ab370a
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2023-03-19 22:43:25 -0300

    props included - minute 2.26

--- testimonios-freecodecamp/src/components/Testimonio.jsx
+++ testimonios-freecodecamp/src/components/Testimonio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '../stylesheets/Testimonio.css'
 
-function Testimonio(){
+function Testimonio(props){
   return (
     <div className="contenedor-testimonio">
       <img 
@@ -9,9 +9,13 @@ function Testimonio(){
         src={require('../img/testimonio-maxi.png')}
         alt='Foto de Emma' />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"> Maximiliano Rivero Zuin de Coquimbito</p>
-        <p className="cargo-testimonio">Ingeniero de Software en Code Ink</p>
-        <p className="texto-testimonio">I absolutely loved working for XYZ Software. The team was incredibly talented and supportive, and I learned so much from my colleagues. The company culture was amazing too, with regular team-building events and opportunities for professional development. I feel like I grew both personally and professionally during my time there.</p>
+        <p className="nombre-testimonio"> <b>{props.nombre}</b> de {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">{props.testimonio}</p>
+      
+        {/* <p className="nombre-testimonio"> Maximiliano Rivero Zuin de Coquimbito</p> */}
+        {/* <p className="cargo-testimonio">Ingeniero de Software en Code Ink</p> */}
+        {/* <p className="texto-testimonio">"I absolutely loved working for XYZ Software. The team was incredibly talented and supportive, and I learned so much from my colleagues. The company culture was amazing too, with regular team-building events and opportunities for professional development. I feel like I grew both personally and professionally during my time there."</p> */}
       </div>
     </div>
   )
