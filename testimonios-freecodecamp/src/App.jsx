c950f22c0e0d80976c44e06c5f788b16cc30f1f5
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2023-03-20 12:53:32 -0300

    testimonios project finished

--- testimonios-freecodecamp/src/App.jsx
+++ testimonios-freecodecamp/src/App.jsx
@@ -2,11 +2,36 @@ import './App.css';
 import Testimonio from './components/Testimonio';
 
 function App() {
+
+  const cargo = 'Software Engineer'
+  const empresa = 'Code Ink'
+
   return (
     <div className="App">
+      <h1 className='codeink'>Code Ink</h1> 
       <div className="contenedor-principal">
-        <h1>Esto es lo que dicen nuestros trabajadores sobre Code Ink:</h1>
-       <Testimonio/> 
+        <h1>This is what our employees say about us:</h1>
+        <Testimonio
+          nombre='Maximiliano Rivero Zuin'
+          pais='Coquimbito'
+          imagen='maximiliano'
+          cargo = {cargo}
+          empresa = {empresa}
+          testimonio='I absolutely loved working for Code Ink. The team was incredibly talented and supportive, and I learned so much from my colleagues. The company culture was amazing too, with regular team-building events and opportunities for professional development. I feel like I grew both personally and professionally during my time there.' /> 
+        <Testimonio
+          nombre='Franco Gregorio'
+          pais='Gutierrez'
+          imagen='franco'
+          cargo = {cargo}
+          empresa = {empresa}
+          testimonio='I can honestly say that my time at Code Ink was one of the best experiences of my professional career. The company was truly invested in its employees, providing ongoing training and development opportunities, as well as a supportive and inclusive work environment. The team was like a family, and we all worked together towards a common goal. I was able to make a meaningful contribution to the company success while also growing as an individual.' />
+        <Testimonio
+          nombre='Lucas Udrea'
+          pais='Las Heras'
+          imagen='lucas'
+          cargo = {cargo}
+          empresa = {empresa}
+          testimonio='I absolutely loved working for Code Ink. The team was incredibly talented and supportive, and I learned so much from my colleagues. The company culture was amazing too, with regular team-building events and opportunities for professional development. I feel like I grew both personally and professionally during my time there.' /> 
       </div>
     </div>
   );
--- testimonios-freecodecamp/src/components/Testimonio.jsx
+++ testimonios-freecodecamp/src/components/Testimonio.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import '../stylesheets/Testimonio.css'
 
 function Testimonio(props){
@@ -6,16 +5,18 @@ function Testimonio(props){
     <div className="contenedor-testimonio">
       <img 
         className="imagen-testimonio" 
-        src={require('../img/testimonio-maxi.png')}
-        alt='Foto de Emma' />
+        src={require(`../img/testimonio-${props.imagen}.png`)}
+        alt={`Foto de ${props.nombre}`} />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"> <b>{props.nombre}</b> de {props.pais}</p>
-        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
-        <p className="texto-testimonio">{props.testimonio}</p>
-      
-        {/* <p className="nombre-testimonio"> Maximiliano Rivero Zuin de Coquimbito</p> */}
-        {/* <p className="cargo-testimonio">Ingeniero de Software en Code Ink</p> */}
-        {/* <p className="texto-testimonio">"I absolutely loved working for XYZ Software. The team was incredibly talented and supportive, and I learned so much from my colleagues. The company culture was amazing too, with regular team-building events and opportunities for professional development. I feel like I grew both personally and professionally during my time there."</p> */}
+        <p className="nombre-testimonio"> 
+          <strong>{props.nombre}</strong> from {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} at <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">
+          "{props.testimonio}"
+        </p>
       </div>
     </div>
   )
